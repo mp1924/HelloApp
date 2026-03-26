14d97856c47bb300501172c688dcad7b4c48395e
mp1924 <mp1924@srmist.edu.in>
2026-03-26 11:00:34 +0530

    Update HelloApp.java

--- src/main/HelloApp.java
+++ src/main/HelloApp.java
@@ -1,14 +1,22 @@
 
 public class HelloApp {
     public static void main(String[] args) {
-        String name;
 
-        if (args.length > 0) {
-            name = args[0];
+        String result;
+
+        if (args.length == 0) {
+            result = "World";
         } else {
-            name = "World";
+            StringBuilder sb = new StringBuilder();
+
+            for (String name : args) {
+                sb.append(name).append(", ");
+            }
+
+            // Remove last ", "
+            result = sb.substring(0, sb.length() - 2);
         }
 
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + result + "!");
     }
 }
